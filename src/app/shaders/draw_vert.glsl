8f4cfb1647b1f0646e455b350fd1a4161900c9e8
Joseph Micheli <josephwmicheli@gmail.com>
2024-01-04 20:35:17 -0600

    Revert "Move shader source code to separate files"
    This reverts commit 5c5267e62ed31b61c18f24602168e6f50f6e5df6.

--- src/app/shaders/draw_vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 300 es
-precision highp float;
-layout(location = 0) in vec2 a_pos;
-out vec2 v_texcoord;
-
-void main() {
-  gl_Position = vec4(a_pos, 0.0, 1.0);
-  v_texcoord = vec2((a_pos.x + 1.0)/2.0, (a_pos.y + 1.0)/2.0);
-}
