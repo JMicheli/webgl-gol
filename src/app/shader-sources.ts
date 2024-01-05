5c5267e62ed31b61c18f24602168e6f50f6e5df6
Joseph Micheli <josephwmicheli@gmail.com>
2024-01-04 20:01:50 -0600

    Move shader source code to separate files

--- src/app/shader-sources.ts
+++ src/app/shader-sources.ts
@@ -1,94 +1,28 @@
 import { ShaderSource } from "./shader";
 
 // Vertex shader for update call
-const UpdateVertexSource = `#version 300 es
-layout(location = 0) in vec2 a_pos;
-
-void main() {
-  gl_Position = vec4(a_pos, 0.0, 1.0);
-}`;
-
+import UpdateVertexSource from "./shaders/update_vert.glsl?raw";
 // Fragment shader for updating grid
-const UpdateFragmentSource = `#version 300 es
-precision highp float;
-uniform sampler2D u_state;
-uniform vec2 u_scale;
-out vec4 color;
-
-int cell_value(vec2 offset) {
-  //Compute uvs from pixel space coordinates
-  vec2 state_coords = (gl_FragCoord.xy + offset) / u_scale;
-  return int(texture(u_state, state_coords));
-}
-
-int neighbors() {
-  int count = 0;
-
-  //Sum up all states
-  count += cell_value(vec2(0,  1));
-  count += cell_value(vec2(1,  1));
-  count += cell_value(vec2(1,  0));
-  count += cell_value(vec2(1, -1));
-  count += cell_value(vec2(0, -1));
-  count += cell_value(vec2(-1,-1));
-  count += cell_value(vec2(-1, 0));
-  count += cell_value(vec2(-1, 1));
-
-  return count;
-}
-
-void main() {
-  //Grab number of neighbors
-  int n = neighbors();
-
-  if (n < 2) {
-    color = vec4(0.0, 0.0, 0.0, 1.0);
-    return;
-  }
-  if (n > 3) {
-    color = vec4(0.0, 0.0, 0.0, 1.0);
-    return;
-  }
-  if (n == 3) {
-    color = vec4(1.0, 1.0, 1.0, 1.0);
-    return;
-  }
-  else {
-    float state_color = float(cell_value(vec2(0.0, 0.0)));
-    color = vec4(state_color, state_color, state_color, 1.0);
-    return;
-  }
-}`;
+import UpdateFragmentSource from "./shaders/update_frag.glsl?raw";
 
 // Vertex shader for draw call
-const DrawVertexSource = `#version 300 es
-precision highp float;
-layout(location = 0) in vec2 a_pos;
-out vec2 v_texcoord;
-
-void main() {
-  gl_Position = vec4(a_pos, 0.0, 1.0);
-  v_texcoord = vec2((a_pos.x + 1.0)/2.0, (a_pos.y + 1.0)/2.0);
-}`;
-
+import DrawVertexSource from "./shaders/draw_vert.glsl?raw";
 // Fragment shader coloring grid on draw call
-const DrawFragmentSource = `#version 300 es
-precision highp float;
+import DrawFragmentSource from "./shaders/draw_frag.glsl?raw";
 
-in vec2 v_texcoord;
-
-uniform sampler2D u_texture;
-out vec4 color;
-
-void main() {
-  color = texture(u_texture, v_texcoord);
-}`;
-
+/**
+ * Source code for the update program, which is responsible for
+ * updating the state of Conway's Game of Life at each iteration.
+ */
 export const UpdateSource: ShaderSource = {
   Vertex: UpdateVertexSource,
   Fragment: UpdateFragmentSource
 };
 
+/**
+ * Source code for the draw program, which is responsible for
+ * drawing the state of the game to the screen on each frame.
+ */
 export const DrawSource: ShaderSource = {
   Vertex: DrawVertexSource,
   Fragment: DrawFragmentSource
